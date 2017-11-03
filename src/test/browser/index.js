e9e05010a2b8815db2ed2c26fc97a39fe27906e9
Angus Croll <angus@uber.com>
2017-11-02 17:32:59 -0700

    cp

--- src/test/browser/index.js
+++ src/test/browser/index.js
@@ -2,9 +2,37 @@
 
 import tape from 'tape';
 
-tape('Simple test', t => {
-  t.deepEqual(Object.assign({a: 2}, {b: 5}), {a: 2, b: 5});
-  t.ok(typeof fetch === 'function');
+/*
+See https://github.com/zloirock/core-js#commonjs
+*/
 
+tape('ES5', t => {
+  t.doesNotThrow(() => {
+    Object.create({a: 2});
+  }, 'Object.create');
+  t.doesNotThrow(() => {
+    Object.keys({a: 2});
+  }, 'Object.keys');
+  t.doesNotThrow(() => {
+    Array.isArray([]);
+  }, 'Array.isArray');
+  t.end();
+});
+
+tape('ES6', t => {
+  t.doesNotThrow(() => {
+    Object.assign({a: 2}, {b: 3});
+  }, 'Object.assign');
+  t.doesNotThrow(() => {
+    new Promise(res => res()).then(() => {});
+  }, 'Promise');
+  t.doesNotThrow(() => {
+    [1, 2, 3].find(e => e === 2);
+  }, 'Array#find');
+  t.end();
+});
+
+tape('W3C', t => {
+  t.ok(typeof fetch === 'function', 'fetch');
   t.end();
 });
