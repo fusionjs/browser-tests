28eb437ed493dd6e13348597b409f8b133b390fe
Alexander Smith <amsmith@uber.com>
2018-01-22 13:41:23 -0800

    Add short MIT license header to all source files

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export default (state, action) => {
   if (action.type.endsWith('_SUCCESS')) return '';
   return action.type.endsWith('_FAILURE') ? action.payload : state;
