a8786d99704919eee418db301f0d4b03bb046a81
Kevin Grandon <kevingrandon@yahoo.com>
2018-04-17 10:17:55 -0700

    Remove code fence around favicon (#120)
    This should no longer be needed due to the pure annotation we now perform.

--- src/plugins/favicon.js
+++ src/plugins/favicon.js
@@ -7,18 +7,17 @@ this plugin will be completely removed in favor of a better interface
 import {assetUrl, dangerouslySetHTML, createPlugin} from 'fusion-core';
 import type {Context} from 'fusion-core';
 
-export default __NODE__ &&
-  createPlugin({
-    middleware: () => {
-      const iconUrl = assetUrl('../static/favicon.ico');
-      const escaped = dangerouslySetHTML(
-        `<link rel="shortcut icon" type="image/x-icon" href="${iconUrl}"><link rel="icon" type="image/x-icon" href="${iconUrl}">`
-      );
-      return (ctx: Context, next) => {
-        if (ctx.element) {
-          ctx.template.head.push(escaped);
-        }
-        return next();
-      };
-    },
-  });
+export default createPlugin({
+  middleware: () => {
+    const iconUrl = assetUrl('../static/favicon.ico');
+    const escaped = dangerouslySetHTML(
+      `<link rel="shortcut icon" type="image/x-icon" href="${iconUrl}"><link rel="icon" type="image/x-icon" href="${iconUrl}">`
+    );
+    return (ctx: Context, next) => {
+      if (ctx.element) {
+        ctx.template.head.push(escaped);
+      }
+      return next();
+    };
+  },
+});
