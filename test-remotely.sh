d8f3f729cc7921d7dc3f8015c4c236528e3d1bf3
Kevin Grandon <kevingrandon@yahoo.com>
2018-10-17 13:51:03 -0700

    Update Sauce Labs environments
    Update Sauce Labs environments to remove legacy/deprecated browsers.

--- test-remotely.sh
+++ test-remotely.sh
@@ -9,15 +9,15 @@ then
 
   nightwatch --config nightwatch-ci.js --env chrome-mac
   nightwatch --config nightwatch-ci.js --env chrome-windows
-  nightwatch --config nightwatch-ci.js --env safari-7
   nightwatch --config nightwatch-ci.js --env safari-9
   nightwatch --config nightwatch-ci.js --env safari-11
+  nightwatch --config nightwatch-ci.js --env safari-12
   nightwatch --config nightwatch-ci.js --env ie-9
   nightwatch --config nightwatch-ci.js --env ie-11
   nightwatch --config nightwatch-ci.js --env edge
   nightwatch --config nightwatch-ci.js --env samsung-S3
   nightwatch --config nightwatch-ci.js --env samsung-G4
-  nightwatch --config nightwatch-ci.js --env iPhone-4S
+  nightwatch --config nightwatch-ci.js --env iPhone-5C
   nightwatch --config nightwatch-ci.js --env iPhone-X
   nightwatch --config nightwatch-ci.js --env kindleFire
 else
