f26cc7dca06f18177c229fbc135846c30f8bcf8c
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-11-15 10:57:41 -0800

    Update browser list, upgrade i18n plugin

--- test-remotely.sh
+++ test-remotely.sh
@@ -7,18 +7,15 @@ then
 
   set -ex
 
-  nightwatch --config nightwatch-ci.js --env chrome-mac
-  nightwatch --config nightwatch-ci.js --env chrome-windows
-  nightwatch --config nightwatch-ci.js --env safari-9
+  nightwatch --config nightwatch-ci.js --env safari-10-3
   nightwatch --config nightwatch-ci.js --env safari-11
   nightwatch --config nightwatch-ci.js --env safari-12
-  nightwatch --config nightwatch-ci.js --env ie-9
+  nightwatch --config nightwatch-ci.js --env edge-latest
+  nightwatch --config nightwatch-ci.js --env firefox-latest
+  nightwatch --config nightwatch-ci.js --env firefox-previous
+  nightwatch --config nightwatch-ci.js --env firefox-46
   nightwatch --config nightwatch-ci.js --env ie-11
-  nightwatch --config nightwatch-ci.js --env edge
-  nightwatch --config nightwatch-ci.js --env samsung-S3
-  nightwatch --config nightwatch-ci.js --env samsung-G4
-  nightwatch --config nightwatch-ci.js --env iPhone-5s
-  nightwatch --config nightwatch-ci.js --env kindleFire
+
 else
   echo "### running tests against $1"
   nightwatch --config nightwatch-ci.js --env $1
